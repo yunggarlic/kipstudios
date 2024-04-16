85b000673cb4499f352448a4f5e02c3c2d9d43ab
Tim Ferrari <timferrari@Tims-MacBook-Pro.local>
2024-04-16 00:28:59 -0500

    updates testimonial arrow rendering

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -9,12 +9,14 @@ const Testimonials = ({ testimonials }: TestimonialsProps) => {
     <section className="flex flex-col w-full items-center gap-10">
       <h2>Testimonials</h2>
       <div className="flex flex-col items-center w-full gap-4">
-        <div className="relative flex items-center gap-4 desktop:gap-10 w-full desktop:w-1/2">
-          <div className="swiper-button-prev !hidden !desktop:block"></div>
+        <div className="relative flex items-center gap-4 desktop:gap-10 w-full desktop:w-3/5">
+          <div className="swiper-button-prev !hidden desktop:!block" />
           <Carousel
             swiperOptions={{
               height: 300,
               centeredSlides: true,
+              slidesPerView: 1.2,
+              spaceBetween:18,
               autoplay: {
                 delay: 5000,
               },
@@ -46,7 +48,7 @@ const Testimonials = ({ testimonials }: TestimonialsProps) => {
               />
             ))}
           </Carousel>
-          <div className="swiper-button-next !hidden !desktop:block"></div>
+          <div className="swiper-button-next !hidden desktop:!block" />
         </div>
         <div className="flex justify-center swiper-pagination-custom desktop:hidden !static" />
       </div>
@@ -59,7 +61,7 @@ const Testimonial = ({
 }: TestimonialProps) => {
   return (
     <div className="h-full flex items-center justify-center p-1">
-      <div className="flex flex-col justify-between w-full h-full desktop:min-w-[400px] p-8 button-default rounded">
+      <div className="flex flex-col justify-between w-full h-full desktop:min-w-[325px] p-8 button-default rounded">
         <div className="pb-4">
           <div className="w-fit rounded">
             <p className="w-fit p-2 bg-yellow-200 dark:text-black font-bold rounded-xl ">
