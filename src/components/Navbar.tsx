fd7acd85c1758346782aba03ef5d75009df5a6bd
Tim <ferrari.tim.v@gmail.com>
2024-04-21 00:35:34 -0500

    adds navbar hide and reveal on scroll

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,59 @@
 "use client";
 import Link from "next/link";
 import { BookNow } from "./";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const [scrollDelta, setScrollDelta] = useState(0);
+  const [isHidden, setIsHidden] = useState(false);
+  useEffect(() => {
+    const handleWheel = (e: WheelEvent) => {
+      const deltaY = e.deltaY;
+      if (deltaY < 0) {
+        setScrollDelta(0);
+        return;
+      }
+      setScrollDelta((prev) => prev + deltaY);
+    };
+
+    window.addEventListener("wheel", handleWheel);
+    return () => window.removeEventListener("wheel", handleWheel);
+  }, []);
+
+  useEffect(() => {
+    if (scrollDelta <= 0) {
+      setIsHidden(false);
+      return;
+    }
+    if (scrollDelta >= 96) {
+      setIsHidden(true);
+      return;
+    }
+  }, [scrollDelta]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const nav = document.querySelector("nav");
+      if (window.scrollY > 0) {
+        nav?.classList.add("shadow-md");
+      } else {
+        nav?.classList.remove("shadow-md");
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <nav className="absolute top-0 left-0 w-full h-24 flex justify-between items-center gap-10 container-default">
-      <Home />
-      {/* <NavLinks /> */}
-      <BookNow />
+    <nav
+      className={`transition-all duration-200 sticky z-20 top-0 left-0 w-full h-24 bg-white dark:bg-black ${
+        isHidden ? "top-[-6rem]" : ""
+      }`}
+    >
+      <div className="checkerboard-default w-full h-full flex justify-between items-center gap-10 container-default">
+        <Home />
+        {/* <NavLinks /> */}
+        <BookNow />
+      </div>
     </nav>
   );
 };
