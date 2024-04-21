42861521b47027404460b78295e45161e666238d
Tim <ferrari.tim.v@gmail.com>
2024-04-21 00:36:02 -0500

    adds default container padding

--- src/components/ScheduleTour.tsx
+++ src/components/ScheduleTour.tsx
@@ -1,6 +1,6 @@
 const ScheduleTour = () => {
   return (
-    <div className="w-full container-default flex flex-col items-center gap-10">
+    <div className="w-full py-10 desktop:py-20 container-default flex flex-col items-center gap-10">
       <h2>Not sure if the space fits your needs?</h2>
       <a
         target="_blank"
