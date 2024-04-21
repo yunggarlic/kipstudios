74d3c68ad5d6d7db2c9343ffb14cf8531b974e7f
Tim <ferrari.tim.v@gmail.com>
2024-04-21 00:37:31 -0500

    adds dynamic image file read

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,26 @@ import {
   Amenities,
   Carousel,
   ContentSplit,
+  Hero,
+  Rooms,
   Testimonials,
   ScheduleTour,
   SpaceFeature as Feature,
   ImageSlide,
 } from "../components";
 import React from "react";
-
+import fs from "fs";
+import path from "path";
 export default function Home() {
   return (
     <React.Fragment>
+      <Hero />
+      <Amenities />
       <ContentSplit>
         <LeftColumn />
         <RightColumn />
       </ContentSplit>
-      <Amenities />
+      <Rooms />
       <Testimonials testimonials={testimonials} />
       <ScheduleTour />
     </React.Fragment>
@@ -42,7 +47,6 @@ const LeftColumn = () => {
   return (
     <div className="relative order-2 desktop:order-1 desktop:w-1/2">
       <div className="desktop:sticky top-10 flex flex-col desktop:h-full gap-10">
-        <Heading />
         <div className="flex flex-col justify-around gap-4 h-full">
           {feats.map((feat) => (
             <Feature key={feat.heading} {...feat} />
@@ -53,16 +57,20 @@ const LeftColumn = () => {
   );
 };
 
-const RightColumn = () => {
+const RightColumn = async () => {
+  const imgSubDir = "/images/studio-imgs/whiteroom";
+  const imageDirectory = path.join(process.cwd(), "public", imgSubDir);
+  const images = fs.readdirSync(imageDirectory);
+  console.log(images);
   return (
     <div className="desktop:w-1/2 h-auto order-1 desktop:order-2">
       <Carousel className="h-full" lazy={true}>
-        {[1, 2, 3, 4, 5].map((i, index) => {
+        {images.map((imgPath, index) => {
           return (
             <ImageSlide
-              key={i}
+              key={imgPath}
               i={index}
-              src={`/images/kip-profile${i}.webp`}
+              src={path.join(imgSubDir, imgPath)}
             />
           );
         })}
