6985a2b99f6ed8cfaa68959186a5fdb75601ce25
Tim <ferrari.tim.v@gmail.com>
2024-04-25 20:56:31 -0500

    removes unused font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Barlow, Montserrat } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
