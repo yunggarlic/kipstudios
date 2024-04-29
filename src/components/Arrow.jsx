ec0fc424bbcacb4c241a57654548fa6fc955c703
Tim <ferrari.tim.v@gmail.com>
2024-04-28 22:53:40 -0500

    adds arrow component and svg

---
+++ src/components/Arrow.jsx
@@ -0,0 +1,27 @@
+const Arrow = () => (
+  <svg className="overflow-visible w-[18px] h-[18px] hover:w-[36px] mr-2 transition-all">
+    <defs>
+      <marker
+        id="m"
+        markerWidth="4"
+        markerHeight="8"
+        refX="0"
+        refY="1"
+        viewBox="0 0 1 2"
+      >
+        <polygon points="0,0 1,1 0,2" fill="black" />
+      </marker>
+    </defs>
+    <line
+      x1="0"
+      y1="50%"
+      x2="100%"
+      y2="50%"
+      stroke-width="2"
+      marker-end="url(#m)"
+      stroke="black"
+    />
+  </svg>
+);
+
+export default Arrow;
