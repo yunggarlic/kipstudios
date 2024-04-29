0d60b4af6df2f9d124542d7c41e60ff78542d971
Tim <ferrari.tim.v@gmail.com>
2024-04-28 22:54:51 -0500

    adds mobile breakpoint

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -7,14 +7,20 @@ import Image from "next/image";
 const Gallery = ({ numImages, children }: GalleryProps) => {
   return (
     <Carousel
-      className="h-screen w-3/4"
+      className="h-screen desktop:h-screen w-3/4"
       swiperOptions={{
         spaceBetween: 30,
-        slidesPerView: 2,
+        slidesPerView: 1,
         grid: { rows: 2 },
         threshold: 10,
         loop: true,
         modules: [Grid],
+        breakpoints:{
+          1024: {
+            slidesPerView: 2,
+            grid: { rows: 2 },
+          },
+        }
       }}
     >
       {children}
