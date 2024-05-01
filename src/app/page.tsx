b475b0c21874aba6e2d6e69042d6c4e50d66754c
Tim <ferrari.tim.v@gmail.com>
2024-04-30 20:37:08 -0500

    adds more images to gallery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,22 +16,30 @@ import React from "react";
 import fs from "fs";
 import path from "path";
 export default function Home() {
-  const imgSubDir = "/images/studio-imgs/whiteroom";
-  const imageDirectory = path.join(process.cwd(), "public", imgSubDir);
-  const images = fs.readdirSync(imageDirectory);
+  const imgSubDirs = [
+    "/images/studio-imgs/whiteroom",
+    "/images/studio-imgs/boudoir",
+    "/images/studio-imgs/checkerboard",
+  ];
+  const imageDirectories = imgSubDirs.map((imgSubDir) =>
+    path.join(process.cwd(), "public", imgSubDir)
+  );
+
+  const images = imageDirectories.flatMap((imageDirectory, i) => [
+    ...fs
+      .readdirSync(imageDirectory)
+      .map((img) => path.join(imgSubDirs[i], img)),
+  ]);
+
   return (
     <React.Fragment>
       <Hero />
       <Amenities />
       <div className="w-full container-default py-10 desktop:py-20">
-        <Gallery className="h-full" lazy={true}>
+        <Gallery className="h-full">
           {images.map((imgPath, index) => {
             return (
-              <ImageSlide
-                key={imgPath}
-                i={index}
-                src={path.join(imgSubDir, imgPath)}
-              />
+              <ImageSlide key={imgPath} i={index} src={path.join(imgPath)} />
             );
           })}
         </Gallery>
