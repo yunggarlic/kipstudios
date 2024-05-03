b02a7052c3afdae04291c917e40b4f129756ddfd
Tim <ferrari.tim.v@gmail.com>
2024-05-02 20:05:25 -0500

    updates img paths

--- src/components/Amenities/content.ts
+++ src/components/Amenities/content.ts
@@ -4,7 +4,7 @@ export const amenitiesContent = [
     description:
       "Our primary space boasts year-round ambient lighting for your shoot, equipped with wall-mounted rolling paper backdrops in a rainbow of colors, vintage designer furniture, tables and chairs.",
     imgPaths: [1, 2, 3, 4, 5].map(
-      (i) => `/images/studio-imgs/whiteroom/${i}.jpg`
+      (i) => `/images/studio-imgs/whiteroom/${i}.webp`
     ),
     checkerboardColors: `bg-checkerboard-green bg-green-200`,
   },
@@ -12,14 +12,14 @@ export const amenitiesContent = [
     heading: "Floor to ceiling checkerboard",
     description:
       "Our floor to ceiling checkerboard is a unique feature that you won't find anywhere else. The bold contrast and geometric pattern naturally enhances the vibrancy and energy of the scenes, making your production more engaging and memorable.",
-    imgPaths: [1, 2, 3].map((i) => `/images/studio-imgs/checkerboard/${i}.jpg`),
+    imgPaths: [1, 2, 3].map((i) => `/images/studio-imgs/checkerboard/${i}.webp`),
     checkerboardColors: `bg-checkerboard-red bg-red-200`,
   },
   {
     heading: "Boudoir Suite",
     description:
       "A 12 foot vaulted ceiling themed boudoir suite with a rare queen-sized circular bed, vintage vanity, and a full-length mirror, perfect for intimate portraits and lifestyle shoots.",
-    imgPaths: [1, 2].map((i) => `/images/studio-imgs/boudoir/${i}.jpg`),
+    imgPaths: [1, 2].map((i) => `/images/studio-imgs/boudoir/${i}.webp`),
     checkerboardColors: `bg-checkerboard-blue bg-blue-200`,
   },
 ];
