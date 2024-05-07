ecbccf58bead16c4c95947eeeb3b37d8fd97e16d
Tim <ferrari.tim.v@gmail.com>
2024-05-06 20:15:49 -0500

    adjusts y padding

--- src/components/ScheduleTour/ScheduleTour.tsx
+++ src/components/ScheduleTour/ScheduleTour.tsx
@@ -1,6 +1,6 @@
 const ScheduleTour = () => {
   return (
-    <div className="w-full py-10 desktop:py-20 container-default flex flex-col items-center gap-10">
+    <div className="w-full py-4 desktop:py-10 container-default flex flex-col items-center gap-10">
       <h2 className="text-center">Not sure if the space fits your needs?</h2>
       <a
         target="_blank"
