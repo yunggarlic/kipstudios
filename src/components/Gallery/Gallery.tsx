eeb186de8aa7413f9a55386341051741368f7d73
Tim <ferrari.tim.v@gmail.com>
2024-05-06 20:15:24 -0500

    updates images and carousel heights

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -4,27 +4,29 @@ import { Grid } from "swiper/modules";
 import "swiper/css/grid";
 import Image from "next/image";
 
-const Gallery = ({ children }: GalleryProps) => {
+const Gallery = ({ children, className }: GalleryProps) => {
   return (
-    <Carousel
-      className="h-screen desktop:h-screen w-3/4"
-      swiperOptions={{
-        spaceBetween: 30,
-        slidesPerView: 1,
-        grid: { rows: 2 },
-        threshold: 10,
-        loop: true,
-        modules: [Grid],
-        breakpoints: {
-          1024: {
-            slidesPerView: 2,
-            grid: { rows: 2 },
+    <div className={className}>
+      <Carousel
+        className="desktop:h-screen w-3/4"
+        swiperOptions={{
+          spaceBetween: 30,
+          slidesPerView: 1,
+          grid: { rows: 1 },
+          threshold: 10,
+          loop: true,
+          modules: [Grid],
+          breakpoints: {
+            1024: {
+              slidesPerView: 2,
+              grid: { rows: 2 },
+            },
           },
-        },
-      }}
-    >
-      {children}
-    </Carousel>
+        }}
+      >
+        {children}
+      </Carousel>
+    </div>
   );
 };
 
--- src/components/ImageSlide/ImageSlide.tsx
+++ src/components/ImageSlide/ImageSlide.tsx
@@ -1,18 +1,23 @@
 import Image from "next/image";
-const ImageSlide = ({ src, i, fill = true }: ImageSlideProps) => {
+const ImageSlide = ({
+  src,
+  i,
+  fill = true,
+  className = "",
+  containerClassName,
+}: ImageSlideProps) => {
   return (
-    <div className="h-full overflow-hidden rounded">
+    <div className={`${containerClassName} overflow-hidden rounded`}>
       <Image
-        className="h-full w-full object-cover aspect-square"
-        fill={fill}
+        className={`h-full w-full object-cover aspect-square ${className}`}
+        width={1200}
+        height={800}
         src={src}
         priority={i === 0 ? true : false}
         loading={i === 0 ? "eager" : "lazy"}
         alt="A large loft showing a floor to ceiling checkerboard room, and a tall white-painted loft."
       />
-      {i > 0 && (
-        <div className="swiper-lazy-preloader"></div>
-      )}
+      {i > 0 && <div className="swiper-lazy-preloader"></div>}
     </div>
   );
 };
